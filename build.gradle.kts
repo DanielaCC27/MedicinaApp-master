361c1b672a543a1cc5773b86801b09db8b3f2538
DanielaCC27 <dcastanedac@uqvirtual.edu.co>
2025-10-19 19:03:50 -0500

    Modificación BD

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     // Driver JDBC para Oracle (versión 21.x).TE MU
     //implementation("com.oracle.database.jdbc:ojdbc11:23.2.0.0")
+    implementation("org.postgresql:postgresql:42.7.4")
     runtimeOnly("org.postgresql:postgresql:42.7.4")
 
     // ==== Utilidades ====
